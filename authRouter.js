6a6d65231dddc151529757f2864e97c81ecf8f4f
Aktan-web <idinex@gk.ru>
2021-08-21 14:19:33 +0600

    init project

---
+++ authRouter.js
@@ -0,0 +1,14 @@
+const Router = require('express')
+const router = new Router()
+const controller = require('./authController')
+const {check} = require('express-validator')
+const authMiddleware = require('./middleware/authMiddleware')
+const roleMiddleware = require('./middleware/roleMiddleware')
+router.post('/registration', [
+  check('username', 'Username must not been empty').notEmpty(),
+  check('password', 'Password must be more than 4 characters').isLength({min: 4, max: 10})
+], controller.registration)
+router.post('/login', controller.login)
+router.get('/users', roleMiddleware(['ADMIN']), controller.getUsers)
+
+module.exports = router
